9f8f637ec78dc953efa7b7682ab84618bf2fab8b
ssun0611 <rlathfdl463@naver.com>
2017-04-13 20:57:37 +0900

    #3 layout change

--- app/src/main/java/rlathfdl463/kr/hs/emirim/activitytest/SecondActivity.java
+++ app/src/main/java/rlathfdl463/kr/hs/emirim/activitytest/SecondActivity.java
@@ -1,11 +1,14 @@
 package rlathfdl463.kr.hs.emirim.activitytest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    String tel, name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +16,11 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
         Button butBack=(Button)findViewById(R.id.but_second);
         butBack.setOnClickListener(butBackHandler);
+        Intent intent=getIntent();
+        name=intent.getStringExtra("name");
+        tel=intent.getStringExtra("tel");
+        TextView textResult=(TextView)findViewById(R.id.text_result);
+        textResult.setText("★ NAME:"+name+"\n ★ TEL"+tel);
     }
     View.OnClickListener butBackHandler=new View.OnClickListener() {
         @Override
